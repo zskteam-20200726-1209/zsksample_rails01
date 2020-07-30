7c5bc2d0b14efcd814d594be7de461a490e01b11
高雲大生 <takakumohiorki@takakumodaiseinoMacBook-Air.local>
2020-07-30 17:10:43 +0900

    feat: add Gem FactoryBot RSpec user_spec

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
   validates :email, presence: true
   validates :email, uniqueness: true
   validates :email,
-    format: { with: /\A[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)* \z/ }
+    format: { with: /\A[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*\z/ }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  it "name,emailがある場合" do
+    user = FactoryBot.build(:user)
+    expect(user).to be_valid
+  end
+
+  it "nameが無い場合、無効" do
+    user = FactoryBot.build(:user, name: nil)
+    user.valid?
+    expect(user.errors[:name]).to include("can't be blank")
+  end
+
+  it "emailが無い場合、無効" do
+    user = FactoryBot.build(:user, email: nil)
+    user.valid?
+    expect(user.errors[:email]).to include("can't be blank")
+  end
+
+  it "name,emailどちらも無い場合、無効" do
+    user = FactoryBot.build(:user, name: nil, email: nil)
+    user.valid?
+    expect(user.errors[:name]).to include("can't be blank")
+    expect(user.errors[:email]).to include("can't be blank")        
+  end
+
+  it "emailが重複した場合、無効" do
+    user1 = FactoryBot.create(:user)
+    user2 = FactoryBot.build(:user)
+    user2.valid?
+    expect(user2.errors[:email]).to include("has already been taken")
+  end
+
+  it "emailがアドレス以外の文字入力された場合、無効" do
+    user = FactoryBot.build(:user, email: "12345")
+    user.valid?
+    expect(user.errors[:email]).to include("is invalid") 
+  end
 end
