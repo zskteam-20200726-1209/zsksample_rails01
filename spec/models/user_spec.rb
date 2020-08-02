6884cebc2a153666caa42f697ae2ec2308e059ed
TaHiroki <55903046+TaHiroki@users.noreply.github.com>
2020-08-02 18:24:08 +0900

    Update user_spec.rb
    'emailが重複した場合、無効' 　の部分において、user変数を修正。

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -26,13 +26,10 @@ RSpec.describe User, type: :model do
   end
 
   it 'emailが重複した場合、無効' do
-    user = FactoryBot.create(:user)
-    user2 = FactoryBot.build(:user)
-    user2.valid?
-    # rubocopで「Lint/UselessAssignment: Useless assignment to variable」が出たので一旦出ない様に
-    # 定義した変数を使ってないからuser2ではないかと言われる
-    expect(user).to be_valid
-    expect(user2.errors[:email]).to include('has already been taken')
+    FactoryBot.create(:user)
+    user = FactoryBot.build(:user)
+    user.valid?
+    expect(user.errors[:email]).to include('has already been taken')
   end
 
   it 'emailがアドレス以外の文字入力された場合、無効' do
