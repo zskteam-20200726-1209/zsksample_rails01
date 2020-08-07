ca19960607ab9634c6bb9653cffb5eaef79f3637
mocotan <19368710+mocotanus@users.noreply.github.com>
2020-08-07 23:59:56 +0900

    fix: returns the added test

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -15,49 +15,27 @@ require 'rails_helper'
 RSpec.describe '/users', type: :request do
   # User. As you add validations to User, be sure to
   # adjust the attributes here as well.
-  let(:valid_attributes) { FactoryBot.create(:user) }
-  let(:invalid_attributes) { FactoryBot.build(:user, name: nil, email: 'suzuki@example.com') }
+  let(:valid_attributes) do
+    skip('Add a hash of attributes valid for your model')
+  end
+
+  let(:invalid_attributes) do
+    skip('Add a hash of attributes invalid for your model')
+  end
 
   describe 'GET /index' do
-    before do
-      FactoryBot.create :user
-    end
-
     it 'renders a successful response' do
+      User.create! valid_attributes
       get users_url
       expect(response).to be_successful
-    end
-    it 'リクエストが成功すること' do
-      get users_url
-      expect(response.status).to eq 200
-    end
-    it 'ユーザー名が表示されていること' do
-      get users_url
-      expect(response.body).to include 'taro'
     end
   end
 
   describe 'GET /show' do
-    context 'ユーザーが存在する場合' do
-      let(:user) { FactoryBot.create :user }
-
-      it 'renders a successful response' do
-        get user_url(user)
-        expect(response).to be_successful
-      end
-      it 'リクエストが成功すること' do
-        get user_url user.id
-        expect(response.status).to eq 200
-      end
-      it 'ユーザー名が表示されていること' do
-        get user_url user.id
-        expect(response.body).to include 'taro'
-      end
-    end
-
-    context 'ユーザーが存在しない場合' do
-      subject { -> { get user_url 0 } }
-      it { is_expected.to raise_error ActiveRecord::RecordNotFound }
+    it 'renders a successful response' do
+      user = User.create! valid_attributes
+      get user_url(user)
+      expect(response).to be_successful
     end
   end
 
@@ -66,29 +44,13 @@ RSpec.describe '/users', type: :request do
       get new_user_url
       expect(response).to be_successful
     end
-    it 'リクエストが成功すること' do
-      get new_user_url
-      expect(response.status).to eq 200
-    end
   end
 
   describe 'GET /edit' do
-    let(:user) { FactoryBot.create :user }
     it 'render a successful response' do
+      user = User.create! valid_attributes
       get edit_user_url(user)
       expect(response).to be_successful
-    end
-    it 'リクエストが成功すること' do
-      get edit_user_url user
-      expect(response.status).to eq 200
-    end
-    it 'ユーザー名が表示されていること' do
-      get edit_user_url user
-      expect(response.body).to include 'taro'
-    end
-    it 'メールアドレスが表示されていること' do
-      get edit_user_url user
-      expect(response.body).to include 'taro@example.com'
     end
   end
 
