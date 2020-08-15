d829e00678ef5581f596d8fb10aa3014ab2fa5b3
yoshiki924 <60290916+yoshiki924@users.noreply.github.com>
2020-08-15 12:29:31 +0900

    feat: add image upload function to micropost (#36)

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,6 @@
 class Micropost < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
 
   validates :content, presence: true, length: { maximum: 140 }
   validates :user_id, presence: true
